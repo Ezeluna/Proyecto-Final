1f0f567fa1e5fe7403ae2565112b8dd956f212a7
Ezequiel Luna <Ezequiel Luna@Ezequiel-PC>
2020-03-26 00:42:36 -0400

    Principal CERCARTI.

--- ProyectoFinal/src/visual/Principal.java
+++ ProyectoFinal/src/visual/Principal.java
@@ -1,15 +1,19 @@
 package visual;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JInternalFrame;
+import javax.swing.JLayeredPane;
 
 public class Principal extends JFrame {
 
 	private JPanel contentPane;
+	private Dimension dim = null;
 
 	/**
 	 * Launch the application.
@@ -31,12 +35,17 @@ public class Principal extends JFrame {
 	 * Create the frame.
 	 */
 	public Principal() {
+		setTitle("CERCARTI");
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		dim = super.getToolkit().getScreenSize();
+		super.setSize(dim.width, (dim.height-50));
+		setLocationRelativeTo(null);
 	}
 
 }
