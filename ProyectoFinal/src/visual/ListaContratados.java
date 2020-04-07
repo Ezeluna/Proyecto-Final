39d28a9ee5957ff950701692ce80b2798e54a694
Darianny Escoboza <Darianny Escoboza@DESKTOP-HS8P45H>
2020-04-07 02:05:17 -0400

    Lista contratados

--- ProyectoFinal/src/visual/ListaContratados.java
+++ ProyectoFinal/src/visual/ListaContratados.java
@@ -53,6 +53,7 @@ public class ListaContratados extends JDialog {
 	private static DefaultTableCellRenderer centrar = new DefaultTableCellRenderer();
 	private Empresa miEmpresa; 
 	private JTextField txtName;
+	private JFormattedTextField ftxtRNC;
 
 	/**
 	 * Launch the application.
@@ -96,8 +97,7 @@ public class ListaContratados extends JDialog {
 			model = new DefaultTableModel();
 			table.setModel(model);
 			table.getTableHeader().setResizingAllowed(false);
-			loadTabla();
-			//loadEmpresa();
+			loadTablaG();
 			scrollPane.setViewportView(table);
 		}
 		
@@ -108,11 +108,25 @@ public class ListaContratados extends JDialog {
 		panel.setLayout(null);
 		
 		JButton btnSearch = new JButton("");
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (Bolsa_Laboral.getInstance().RetornarEmpresa(ftxtRNC.getText()) != null) {
+					miEmpresa = Bolsa_Laboral.getInstance().RetornarEmpresa(ftxtRNC.getText());
+					txtName.setText(miEmpresa.getNombre());
+					loadTabla();
+				} 
+				else {
+					JOptionPane.showMessageDialog(null,
+							"No se encontro una empresa con el RNC digitado.", "Información",
+							JOptionPane.WARNING_MESSAGE, null);
+				}
+			}
+		});
 		btnSearch.setIcon(new ImageIcon(ListaContratados.class.getResource("/icons/Logo Buscar.png")));
 		btnSearch.setBounds(281, 11, 29, 21);
 		panel.add(btnSearch);
 		
-		JFormattedTextField ftxtRNC = new JFormattedTextField();
+		 ftxtRNC = new JFormattedTextField();
 		ftxtRNC.setBounds(154, 11, 117, 20);
 		panel.add(ftxtRNC);
 		
@@ -125,11 +139,25 @@ public class ListaContratados extends JDialog {
 		panel.add(lblName);
 		
 		txtName = new JTextField();
+		txtName.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		txtName.setEnabled(false);
 		txtName.setColumns(10);
 		txtName.setBackground(Color.WHITE);
 		txtName.setBounds(154, 38, 430, 20);
 		panel.add(txtName);
+		
+		JButton btnGeneral = new JButton("General");
+		btnGeneral.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				loadTablaG();
+			}
+		});
+		btnGeneral.setBounds(10, 21, 78, 23);
+		panel.add(btnGeneral);
 		
 		MaskFormatter mascara = new MaskFormatter("##########");
 		{
@@ -168,20 +196,21 @@ public class ListaContratados extends JDialog {
 	
 	
 	private  void loadTabla() {
-		String [] columNombres = {"Cédula", "Empleado", "Tipo de empleado", "Teléfono"};
+		String [] columNombres = {"Cédula", "Empleado", "Tipo de empleado", "Fecha Contratado"};
 		model.setColumnIdentifiers(columNombres);
+		Empresa empre = Bolsa_Laboral.getInstance().RetornarEmpresa(ftxtRNC.getText());
 		model.setRowCount(0);
 		fila = new Object[model.getColumnCount()];
-		for(Personal aux : Bolsa_Laboral.getInstance().getMisSolicitantes()) {	
-			if(aux.isContratado()) {
+		if(empre != null) {
+		for(Personal aux : empre.getMisEmpleadosC()) {	
 					fila[0] = aux.getCedula();
 					fila[1] = aux.getName() + " " + aux.getApellido();
 					fila[2] = tipoEmpleado(aux);
-					fila[3] =  aux.getTelefono();
+					fila[3] =  aux.getFechaContratado();
 					model.addRow(fila);
-			   
+			
 	        } 
-		}
+		
 		centrar.setHorizontalAlignment(SwingConstants.CENTER);
 		for (int i = 0; i < columNombres.length; i++) {
 			table.getColumnModel().getColumn(i).setCellRenderer(centrar);
@@ -194,7 +223,42 @@ public class ListaContratados extends JDialog {
 		columnModel.getColumn(1).setPreferredWidth(170);
 		columnModel.getColumn(2).setPreferredWidth(165);
 		columnModel.getColumn(3).setPreferredWidth(167);
-
+	}
+	
+	else{
+		JOptionPane.showMessageDialog(null, "No se encontró ningún solicitud para el RNC dado.",
+				"ATENCIÓN", JOptionPane.ERROR_MESSAGE, null);
+	}
+	}
+	
+	private  void loadTablaG() {
+		String [] columNombres = {"Cédula", "Empleado", "Tipo de empleado", "Fecha Contratado"};
+		model.setColumnIdentifiers(columNombres);
+		model.setRowCount(0);
+		fila = new Object[model.getColumnCount()];
 		
+		for(Personal aux : Bolsa_Laboral.getInstance().getMisSolicitantes()) {	
+			if(aux.isContratado()) {
+					fila[0] = aux.getCedula();
+					fila[1] = aux.getName() + " " + aux.getApellido();
+					fila[2] = tipoEmpleado(aux);
+					fila[3] =  aux.getFechaContratado();
+					model.addRow(fila);
+			}
+	        } 
+		
+		centrar.setHorizontalAlignment(SwingConstants.CENTER);
+		for (int i = 0; i < columNombres.length; i++) {
+			table.getColumnModel().getColumn(i).setCellRenderer(centrar);
+		}
+		table.setModel(model);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.getTableHeader().setReorderingAllowed(false);
+		TableColumnModel columnModel = table.getColumnModel();
+		columnModel.getColumn(0).setPreferredWidth(170);
+		columnModel.getColumn(1).setPreferredWidth(170);
+		columnModel.getColumn(2).setPreferredWidth(165);
+		columnModel.getColumn(3).setPreferredWidth(167);
+	
 	}
 }
